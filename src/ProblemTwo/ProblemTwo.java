58d274958649e37ba4353fb688f15a41a37606aa
Swapnil Jathar <swapneel.jathar@gmail.com>
2016-04-06 09:58:12 +0530

    Project Euler Problem03

--- src/ProblemTwo/ProblemTwo.java
+++ src/ProblemTwo/ProblemTwo.java
@@ -4,24 +4,20 @@ package ProblemTwo;
  * Created by Swapnil on 3/31/2016.
  */
 public class ProblemTwo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         long fib1 = 0;
         long fib2 = 1;
         long result = fib1 + fib2;
         long sum = 0;
 
-        while (result <= 4000000){
-
+        while (result <= 4000000) {
             fib1 = fib2;
             fib2 = result;
             result = fib1 + fib2;
-            
-            if (result % 2 == 0 ){
+
+            if (result % 2 == 0) {
                 sum += result;
             }
-
-
-
         }
         System.out.println(sum);
     }
